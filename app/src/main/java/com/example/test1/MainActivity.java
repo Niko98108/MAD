b15c047dbf3299aff3829bc955a046f608e8f56f
Niko98108 <iamnisal123@gmail.com>
2020-08-25 00:35:06 +0530

    test

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText username,password;
+    Button login,register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.perssonName);
+        password =  findViewById(R.id.TextPassword;
+        login = findViewById(R.id.log);
+        register = findViewById(R.id.reg);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),pro_manage.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
